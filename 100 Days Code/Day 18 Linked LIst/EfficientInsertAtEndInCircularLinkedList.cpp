d770bc11b19acd35b6e86925339adf405146772d
HIMANSHU SINGH <71926216+developerhimanshu@users.noreply.github.com>
2022-02-18 13:49:18 +0530

    Create EfficientInsertAtEndInCircularLinkedList.cpp

---
+++ 100 Days Code/Day 18 Linked LIst/EfficientInsertAtEndInCircularLinkedList.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data=x;
+        next=NULL;  
+    }
+};
+
+Node *insertEnd(Node *head, int data)
+{
+    Node *newNode = new Node(data);
+    if(head == NULL)
+    {
+        newNode->next=newNode;
+        return newNode;
+    }
+    else
+    {
+        newNode->next = head->next;
+        head->next = newNode;
+        int x = head->data;
+        head->data = newNode->data;
+        newNode->data = x;
+
+        return newNode;
+    }
+}
+
+void printList(Node *head)
+{
+    if(head == NULL)
+        return;
+    else
+    {
+        Node *p = head;
+        do{
+            cout<<p->data<<" ";
+            p=p->next;
+        }while(p != head);
+    }
+}
+int main()
+{
+    Node*head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=head;
+    cout<<"Before Inserting at end: "<<endl;
+    printList(head);
+    head=insertEnd(head, 15);
+    cout<<"\nAfter Inserting at end: "<<endl;
+    printList(head);
+    return 0;
+}
