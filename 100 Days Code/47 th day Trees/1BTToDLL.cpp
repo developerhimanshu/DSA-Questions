4468be49a1904600183e0d0df847eb4391c5786f
HIMANSHU SINGH <71926216+developerhimanshu@users.noreply.github.com>
2022-03-23 17:37:48 +0530

    Update 1BTToDLL.cpp

--- 100 Days Code/47 th day Trees/1BTToDLL.cpp
+++ 100 Days Code/47 th day Trees/1BTToDLL.cpp
@@ -1,30 +1,35 @@
-TreeNode *BinaryToDLL(TreeNode *root)
-{
-    if (root == NULL)
-        return root;
-    static TreeNode *prev = NULL;
-    TreeNode *head = BinaryToDLL(root->left);
-    if (prev == NULL)
+ void inorder(TreeNode *root)
     {
-        head = root;
-    }
-    else
-    {
-        root->left = prev;
-        prev->right = root;
-    }
-    prev = root;
-    BinaryToDLL(root->right);
-    return head;
-}
+        if (root == NULL)
+            return;
 
-void printlist(TreeNode *head)
-{
-    TreeNode *curr = head;
-    while (curr != NULL)
-    {
-        cout << curr->data << " ";
-        curr = curr->right;
+        inorder(root->left);
+        if (prev == NULL)
+        {
+            head = root;
+        }
+        else
+        {
+            root->left = prev;
+            prev->right = root;
+        }
+        prev = root;
+        inorder(root->right);
     }
-    cout << endl;
-}
+
+    TreeNode *bToDLL(TreeNode *root)
+    {
+        inorder(root);
+        return head;
+    }
+
+    void printlist(TreeNode *head)
+    {
+        TreeNode *curr = head;
+        while (curr != NULL)
+        {
+            cout << curr->data << " ";
+            curr = curr->right;
+        }
+        cout << endl;
+    }
