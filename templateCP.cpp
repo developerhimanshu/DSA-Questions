e5e2c467207deb0813f82cfe3e8bf49dfcbca90a
HIMANSHU SINGH <71926216+developerhimanshu@users.noreply.github.com>
2022-06-22 11:03:27 +0530

    Create templateCP.cpp

---
+++ templateCP.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+
+const int inf = 1e9 + 7;
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int a) {cerr << a;}
+void _print(char a) {cerr << a;}
+void _print(long long a) {cerr << a;}
+void _print(string a) {cerr << a;}
+void _print(bool a) {cerr << a;}
+void _print(double a) {cerr << a;}
+
+template<class T>void _print(vector<T>v1) {cerr << "[ "; for (T i : v1) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set<T>s1) {cerr << "[ "; for (T i : s1) {_print(i); cerr << " ";} cerr << " ]";}
+
+
+void solve() {
+	
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("Error.txt", "w", stderr);
+#endif
+	int tc = 0, t = 1;
+	// cin >> t;
+	while (tc++ < t) {
+		solve();
+	}
+
+}
