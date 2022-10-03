cd149e65eebe58a93cc9fd9e741ddb34d8e36053
Himanshu Singh <chamyalhemsingh@gmail.com>
2022-10-03 19:29:25 +0530

    673. Number of Longest Increasing Subsequence

---
+++ Dynamic Programming/105.NumberOfLongestIncreasingSubsequence.cpp
@@ -0,0 +1,36 @@
+/*
+Given an integer array nums, return the number of longest increasing subsequences.
+Notice that the sequence has to be strictly increasing.
+
+Example 1:
+
+Input: nums = [1,3,5,4,7]
+Output: 2
+Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+*/
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n, 1);
+        vector<int>cnt(n, 1);
+        int maxi = 1;
+        for(int i = 0; i<n; i++){
+            for(int prev = 0; prev < i; prev ++){
+                if(nums[prev]<nums[i] && 1+dp[prev] > dp[i]){
+                    dp[i] =1+dp[prev];
+                    cnt[i]=cnt[prev];//As we are getting new longest, so we  will not change 
+                }else if(nums[prev]<nums[i] &&1+dp[prev]==dp[i]){
+                    cnt[i]+=cnt[prev];
+                }
+            }
+            maxi = max(maxi, dp[i]);
+        }
+        int res = 0;
+        for(int i = 0; i<n; i++){
+            if(dp[i]==maxi)
+                res += cnt[i];      
+        }
+        return res;
+    }
+};
